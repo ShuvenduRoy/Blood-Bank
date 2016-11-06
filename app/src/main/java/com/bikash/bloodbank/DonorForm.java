250dda49ce304cc7432ba9db4a2ff390b57b08a6
Bikash <bikash11roy@gmail.com>
2016-11-06 19:33:47 +0600

    Donor form created

---
+++ app/src/main/java/com/bikash/bloodbank/DonorForm.java
@@ -0,0 +1,15 @@
+package com.bikash.bloodbank;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class DonorForm extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donor_form);
+    }
+}
