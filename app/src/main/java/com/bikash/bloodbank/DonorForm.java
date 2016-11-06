c40d6b6e38e32352244fd50b0e4caf52e48fe90c
Bikash <bikash11roy@gmail.com>
2016-11-06 20:46:17 +0600

    Donor data added into data base

--- app/src/main/java/com/bikash/bloodbank/DonorForm.java
+++ app/src/main/java/com/bikash/bloodbank/DonorForm.java
@@ -2,13 +2,25 @@ package com.bikash.bloodbank;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+
+import com.google.firebase.database.DatabaseReference;
+
+import static com.bikash.bloodbank.MainActivity.database;
 
 public class DonorForm extends AppCompatActivity {
     Spinner cityChoice;
     Spinner groupChoice;
+
+    EditText Name;
+    EditText Mobile;
+
+    Button Save;
+
 
 
     @Override
@@ -27,5 +39,25 @@ public class DonorForm extends AppCompatActivity {
         String[] group = new String[]{"O+","O-", "A+", "B+","A-", "B-", "AB+", "AB-"};
         ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, group);
         groupChoice.setAdapter(adapter1);
+
+        Name = (EditText) findViewById(R.id.edt_name);
+        Mobile = (EditText) findViewById(R.id.edt_mobileNumber);
+
+        Save = (Button) findViewById(R.id.btn_saveDonor);
+
+        Save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = Name.getText().toString();
+                String city = cityChoice.getSelectedItem().toString();
+                String group = groupChoice.getSelectedItem().toString();
+                String mobile = Mobile.getText().toString();
+
+                Donor donor = new Donor(name,mobile,group,city);
+                DatabaseReference myRef = database.getReference("donors");
+                myRef.child(city).push().setValue(donor);
+            }
+        });
+
     }
 }
