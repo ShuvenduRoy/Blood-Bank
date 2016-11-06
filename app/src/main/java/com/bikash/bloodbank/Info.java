26def7038932d1bb33b32c7c773018b141e0722e
Bikash <bikash11roy@gmail.com>
2016-11-06 19:33:14 +0600

    Info Created

---
+++ app/src/main/java/com/bikash/bloodbank/Info.java
@@ -0,0 +1,13 @@
+package com.bikash.bloodbank;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
--- app/src/main/java/com/bikash/bloodbank/MainActivity.java
+++ app/src/main/java/com/bikash/bloodbank/MainActivity.java
@@ -1,7 +1,11 @@
 package com.bikash.bloodbank;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -9,6 +13,11 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
     public static FirebaseDatabase database;
+
+    Button buttonDonor;
+    Button buttonInfo;
+    public static String donorId;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,35 @@ public class MainActivity extends AppCompatActivity {
         database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("donors");
 
+        /**
+         * Initializing variable
+         */
+        donorId = sharedPreferences.getString("id","no");
+
+
+        /**
+         * Wiring up every thing
+         */
+        buttonInfo = (Button) findViewById(R.id.btn_info);
+        buttonInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Info.class));
+            }
+        });
+
+
+        buttonDonor = (Button) findViewById(R.id.btn_donor_profile);
+        if(donorId.toString().equals("no")){
+            buttonDonor.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(MainActivity.this, DonorForm.class));
+                }
+            });
+        } else {
+
+        }
 
 
     }
