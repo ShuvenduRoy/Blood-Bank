f6739939e2e35abddb599ad4a08bb9edd898af93
Bikash <bikash11roy@gmail.com>
2016-11-06 20:09:28 +0600

    Form layout changed

--- app/src/main/java/com/bikash/bloodbank/DonorForm.java
+++ app/src/main/java/com/bikash/bloodbank/DonorForm.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 public class DonorForm extends AppCompatActivity {
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
